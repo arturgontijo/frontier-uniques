68a03b4a3a8f622fba5e29e34d58bbab95c200f6
Artur Gontijo <arturgontijo@gmail.com>
2022-10-27 18:03:36 -0300

    [substorage] Using argparser and better logs.

--- storage-visualizer/python/README.md
+++ storage-visualizer/python/README.md
@@ -2,5 +2,30 @@
 
 ```shell
 pip install -r requirements.txt
-python3 run.py
+
+python3 substorage --help
+
+usage: substorage [-h] [--url URL] [--module MODULE] [--entry ENTRY] [--params PARAMS [PARAMS ...]]
+
+Dump Substrate Storage
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --url URL             Node websocket url. (default: ws://127.0.0.1:9944)
+  --module MODULE       Storage module. (default: EVM)
+  --entry ENTRY         Storage module entry. (default: AccountStorages)
+  --params PARAMS [PARAMS ...]
+                        Storage module entry params. (default: ['0xc2bf5f29a4384b1ab0c063e1c666f02121b6084a'])
+```
+
+```shell
+# To inspect data of a contract at 0xc2bf5f29a4384b1ab0c063e1c666f02121b6084a
+python substorage --params 0xc2bf5f29a4384b1ab0c063e1c666f02121b6084a
+
+# or for 0xfe5d3c52f7ee9aa32a69b96bfbb088ba0bcd8efc
+python substorage --params 0xfe5d3c52f7ee9aa32a69b96bfbb088ba0bcd8efc
+
+# or for Uniques Module, inspect all items that 5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY
+# owns from collection 0xc2bf5f29a4384b1ab0c063e1c666f02121b6084a
+python substorage --module Uniques --entry Account --params 5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY 0xc2bf5f29a4384b1ab0c063e1c666f02121b6084a
 ```
---
+++ storage-visualizer/python/substorage/__main__.py
@@ -0,0 +1,73 @@
+import argparse
+import json
+
+from substrateinterface import SubstrateInterface
+from substrateinterface.exceptions import StorageFunctionNotFound
+
+
+def query_map_and_print(
+    url='ws://127.0.0.1:9944',
+    module='EVM',
+    entry='AccountStorages',
+    params=None
+):
+    substrate = None
+    try:
+        substrate = SubstrateInterface(url=url)
+
+        result = substrate.query_map(
+            module,
+            entry,
+            params=params or [],
+            page_size=200,
+            max_results=400
+        )
+
+        for r in sorted(result, key=lambda x: x[0]):
+            print(f'{r[0]} -> {r[1]}')
+
+    except StorageFunctionNotFound as e:
+        print(f'\nERROR: {e}')
+        print(f'\nERROR: Try "substorage --metadata"\n')
+
+    except Exception as e:
+        if substrate:
+            result = substrate.query(module, entry)
+            print(result)
+        else:
+            print(f'\nERROR: {e}')
+            print(f'\nERROR: Is you Substrate node up and running at "{url}"?\n')
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Dump Substrate Storage', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--url', help='Node websocket url.', default='ws://127.0.0.1:9944')
+    parser.add_argument('--module', help='Storage module.', default='EVM')
+    parser.add_argument('--entry', help='Storage module entry.', default='AccountStorages')
+    parser.add_argument('--params', help='Storage module entry params.', nargs='+', default=['0xc2bf5f29a4384b1ab0c063e1c666f02121b6084a'])
+    parser.add_argument('--metadata', help='Dump Storage metadata.', action='store_true')
+    args = parser.parse_args()
+
+    if args.metadata:
+        substrate = SubstrateInterface(url=args.url)
+        sub_modules = {}
+        for item in substrate.get_metadata_storage_functions():
+            mod_id = item['module_id']
+            if mod_id in sub_modules:
+                sub_modules[mod_id].append(item['storage_name'])
+            else:
+                sub_modules[mod_id] = [item['storage_name']]
+        for k, v in sub_modules.items():
+            sub_modules[k] = sorted(v)
+        print(json.dumps(sub_modules, indent=2))
+    else:
+        query_map_and_print(
+            url=args.url,
+            module=args.module,
+            entry=args.entry,
+            params=args.params,
+        )
+
+
+if __name__ == '__main__':
+    main()
